9f6a1046b4b90de228b1aefac418bcbc97eb4a8c
cnobg1527 <chenhaiyang023@gmail.com>
2021-01-23 20:44:57 +0800

    happy new 20210123

--- src/main/kotlin/c第三章_类和对象/l第十二节：内联类.kt
+++ src/main/kotlin/c第三章_类和对象/l第十二节：内联类.kt
@@ -1,2 +1,28 @@
 package c第三章_类和对象
 
+interface Run {
+    fun run()
+}
+
+class Cat : Run {
+    fun show() {
+        println("我是一只猫")
+    }
+
+    override fun run() {
+        println("快跑不然要被捉住")
+    }
+}
+
+class Mouse(run: Run) : Run by run{
+    fun show(){
+        println("我是一只老鼠")
+    }
+}
+
+fun main() {
+    val r = Cat()
+    Mouse(r).run()
+    Mouse(r).show()//它会调用自己实现的show方法
+
+}
